8622d831c1a0dc460211805b7352293e981ffb6e
Rokugatsu A <rokugatsua@outlook.co.id>
2020-05-09 11:28:02 +0700

    fix

--- hij/settings.py
+++ hij/settings.py
@@ -123,7 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    (os.path.join(BASE_DIR,'static'))
+    (os.path.join(BASE_DIR,'static')),
     ("css",os.path.join(BASE_DIR,"static","css")),
     ("js", os.path.join(BASE_DIR,"static","js")),
     ("icon", os.path.join(BASE_DIR,"static","icon")),
